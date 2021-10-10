98012a6d26121545e1dc28e297f69fcd333d1895
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-10 14:09:46 +0530

    adding welcome msg

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to program world!");
+            Console.ReadLine();
+        }
+    }
+}
