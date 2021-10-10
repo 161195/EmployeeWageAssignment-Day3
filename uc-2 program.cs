58fd6cff453b3e8ac15ffbeab6ffd8d38eedf56f
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-10 14:34:37 +0530

    adding uc2 employee wage

---
+++ uc-2 program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EmployeeWageAssignment
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //constant
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+            Console.ReadLine();
+        }
+    }
+}
