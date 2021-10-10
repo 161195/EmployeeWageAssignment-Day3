8d26f830164d9e3c4fc05e055f8c364fc58e9e8c
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-10 14:40:19 +0530

    adding uc3 employee part time

--- uc-3 program.cs
+++ uc-3 program.cs
@@ -6,16 +6,28 @@ namespace EmployeeWageAssignment
     {
         static void Main(string[] args)
         {
-            //constant
-            int IS_FULL_TIME = 1;
+            //Constants
+
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-            //variables
+
+            //Variables
+
             int empHrs = 0;
+
             int empWage = 0;
+
             Random random = new Random();
+
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
@@ -24,7 +36,7 @@ namespace EmployeeWageAssignment
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage :" + empWage);
+            Console.WriteLine("Emp Wage:" + empWage);
             Console.ReadLine();
         }
     }
