d5348b9d8bf292f85c937afca68295701063b1ae
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-10 15:02:39 +0530

    adding uc6 total work hours per month

--- uc-6 program.cs
+++ uc-6 program.cs
@@ -8,17 +8,21 @@ namespace EmployeeWageAssignment
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
+
         static void Main(string[] args)
         {
             //Variables
 
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
             //Computation
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
+
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -39,13 +43,14 @@ namespace EmployeeWageAssignment
 
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage :" + empWage);
+                totalEmpHrs += empHrs;
+
+                Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs:" + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage :" + totalEmpWage);
             Console.ReadLine();
-        }
 
+        }
     }
 }
