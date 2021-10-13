e200d5d4f9f19c2f68f47558212483057b0d1be1
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-13 14:07:14 +0530

    adding uc7

--- uc-7 program.cs
+++ uc-7 program.cs
@@ -10,42 +10,42 @@ namespace EmployeeWageAssignment
         public const int NUM_OF_WORKING_DAYS = 2;
         public const int MAX_HRS_IN_MONTH = 10;
 
-        public static void EmpWage(String company, int Emp_Rate_Per_Hrs, int Num_Of_Work_Days, int MaxHrsInMonth)
+        public static void EmpWage()
         {
             //variables
             int empHrs = 0, totalWorkDays = 0;
             int totalEmpHrs = 0;
             while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkDays < NUM_OF_WORKING_DAYS)
             {
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
+                
+                 Random random = new Random();
+                    int empCheck = random.Next(0, 3);
 
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
+                    switch (empCheck)
+                    {
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    totalEmpHrs += empHrs;
+                    Console.WriteLine("Total Employee Hours:" + totalEmpHrs + " " + "Employee Hours:" + empHrs);
                 }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Total Employee Hours:" + totalEmpHrs + " " + "Employee Hours:" + empHrs);
+                int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Total Employee Wage is:" + totalEmpWage);
+        }
+
+            public static void Main(String[] args)
+            {
+                EmpWage();
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage is:" + totalEmpWage);
-        }
 
-        public static void Main(String[] args)
-        {
-            EmpWage("Faurecia", 2, 3, 5);
-            EmpWage("IndoShottle", 5, 40, 6);
-            EmpWage("TCS", 13, 10, 21);
-        }
-
+       
     }
 }
 
