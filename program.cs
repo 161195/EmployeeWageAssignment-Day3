61a1bac9db338e30bcaadb41d99a3ce7035c5124
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-19 22:25:40 +0530

    adding UC9 branch

---
+++ program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Fauresia", 2, 3, 5);
+            empWageBuilder.addCompanyEmpWage("Indoshottle", 5, 40, 6);
+
+
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
