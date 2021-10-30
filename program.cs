11f34dcc69f43524c0a5e6eb559facb436087b33
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-30 11:31:56 +0530

    adding UC11 to remote

---
+++ program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeWageAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an array class object
+            //calling all the necessary methods of different classes
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("indoshottle", 4, 7, 30);
+            empWageBuilder.addCompanyEmpWage("Faurecia", 5, 30, 25);
+            empWageBuilder.addCompanyEmpWage("Titan", 5, 20, 35);
+            empWageBuilder.computeEmpWage();
+            Console.ReadLine();
+        }
+    }
+}
+
